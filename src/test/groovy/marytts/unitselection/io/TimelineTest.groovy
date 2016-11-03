38f0dbd957ee632d5a0490150ef37661166be769
Ingmar Steiner <steiner@coli.uni-saarland.de>
2016-11-03 15:47:08 +0100

    rewrite Timeline unit tests

--- src/test/groovy/marytts/unitselection/io/TimelineTest.groovy
+++ src/test/groovy/marytts/unitselection/io/TimelineTest.groovy
@@ -3,20 +3,33 @@ package marytts.unitselection.io
 import org.testng.annotations.*
 
 class TimelineTest {
-    def timeline
 
-    @BeforeMethod
+    File testResourceDir
+    Timeline timeline
+
+    @BeforeClass
     void setUp() {
+        testResourceDir = new File(System.properties.testResourceDir)
+        def testTimeLineFile = new File(testResourceDir, 'timeline_waveforms.mry')
         timeline = new Timeline()
+        timeline.load(testTimeLineFile)
     }
 
     @Test(expectedExceptions = AssertionError)
     void testCreateFrom() {
-        assert false // not yet implemented
+        def wavDir = new File("$testResourceDir/wav")
+        def wavFiles = wavDir.listFiles({ d, f -> f ==~ /.*\.wav/ } as FilenameFilter)
+        assert wavFiles
+        def pmDir = null // for now
+        def actual = Timeline.createFrom(wavDir, pmDir)
+        assert actual == timeline
     }
 
     @Test(expectedExceptions = AssertionError)
-    void testSaveTo() {
-        assert false // not yet implemented
+    void testWriteTo() {
+        def actual = File.createTempFile('timeline_waveforms-', 'mry')
+        timeline.saveTo(actual)
+        def expected = new File(testResourceDir, 'timeline_waveforms.mry')
+        assert actual == expected
     }
 }
